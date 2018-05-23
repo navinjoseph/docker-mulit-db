7c267ecc2916247fa351010bdaf154dfe230e47d
Alex Miles <alex@studiomile.com>
2018-05-23 10:42:15 -0400

    Add more coins to seed file

--- db/seeds/add_coins.js
+++ db/seeds/add_coins.js
@@ -9,7 +9,19 @@ exports.seed = function (knex, Promise) {
           ticker: 'BTC',
           name: 'Bitcoin',
           usdPrice: '12000',
-          timestamp: new Date().toISOString()
+          timestamp: new Date('2017-12-17T03:24:00').toISOString()
+        },
+        {
+          ticker: 'BTC',
+          name: 'Bitcoin',
+          usdPrice: '13000',
+          timestamp: new Date('2018-01-02T03:24:00').toISOString()
+        },
+        {
+          ticker: 'BTC',
+          name: 'Bitcoin',
+          usdPrice: '14000',
+          timestamp: new Date('2018-01-03T03:24:00').toISOString()
         },
         {
           ticker: 'ETH',
