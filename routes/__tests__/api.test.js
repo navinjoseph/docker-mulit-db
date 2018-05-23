0409249b0141e1ab57329129d1de3a1620baec72
Alex Miles <alex@studiomile.com>
2018-05-22 23:40:14 -0400

    Write api endpoints and tests

--- routes/__tests__/api.test.js
+++ routes/__tests__/api.test.js
@@ -1,9 +1,40 @@
 import request from 'supertest'
 import app from '../../app'
+import winston from 'winston'
+import knex from '../../db'
+
+beforeEach(async () => {
+  try {
+    await knex.migrate.rollback()
+    await knex.migrate.latest()
+    await knex.seed.run()
+  } catch (err) {
+    winston.error(err)
+  }
+})
 
 describe('GET /api', () => {
   it('Should return 200', async () => {
     const response = await request(app).get('/api')
     expect(response.statusCode).toBe(200)
+  })
+})
+
+describe('GET /api/history', () => {
+  it('Should return 200', async () => {
+    const response = await request(app).get(
+      '/api/history?symbol=BTC&timestamp=1527034971544'
+    )
+    expect(response.statusCode).toBe(200)
+  })
+
+  it('Should return 400 if no timestamp', async () => {
+    const response = await request(app).get('/api/history?symbol=BTC')
+    expect(response.statusCode).toBe(400)
+  })
+
+  it('Should return 400 if no symbol', async () => {
+    const response = await request(app).get('/api/history')
+    expect(response.statusCode).toBe(400)
   })
 })
--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,37 @@
 import express from 'express'
+// import winston from 'winston'
+import History from '../db/history'
 
 const router = express.Router()
 
 router.get('/', (req, res) => {
   res.send('api')
+})
+
+router.get('/history', async (req, res) => {
+  try {
+    if (!req.query.symbol) {
+      throw new Error('Symbol is required')
+    }
+
+    if (!req.query.timestamp) {
+      throw new Error('Timestamp is required')
+    }
+
+    const timestamp = new Date(Number(req.query.timestamp))
+
+    const date = await History.query().where('ticker', 'BTC')
+    console.log(date)
+
+    const obj = {
+      symbol: req.query.symbol,
+      timestamp
+    }
+
+    res.json(obj)
+  } catch (err) {
+    res.status(400).send({ error: err.message })
+  }
 })
 
 export default router
