ed7f00d6e6cd6671a93e6f0fe6cc9aaf159adaf0
Alex Miles <alex@studiomile.com>
2018-05-30 10:10:08 -0400

    Get tests passing

--- db/seeds/coin.js
+++ db/seeds/coin.js
@@ -4,12 +4,10 @@ exports.seed = function (knex, Promise) {
     .then(() => {
       return knex('coin').insert([
         {
-          id: 1,
           ticker: 'BTC',
           name: 'Bitcoin'
         },
         {
-          id: 2,
           ticker: 'LTC',
           name: 'Litecoin'
         }
