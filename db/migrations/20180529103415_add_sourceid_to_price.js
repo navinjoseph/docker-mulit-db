285d31b65edc4fc4ef0c1cd8b777c06d9cf197a4
Alex Miles <alex@studiomile.com>
2018-05-29 22:44:03 -0400

    Relate column

--- db/migrations/20180529103415_add_sourceid_to_price.js
+++ db/migrations/20180529103415_add_sourceid_to_price.js
@@ -1,6 +1,9 @@
 exports.up = (knex, Promise) => {
   return knex.schema.table('price', table => {
-    table.integer('source_id')
+    table
+      .integer('source_id')
+      .references('id')
+      .inTable('source')
   })
 }
 
