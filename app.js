f2b2e94dbc36b67eb4ae584d231b570c00ad5e4a
Alex Miles <alex@studiomile.com>
2018-05-30 14:21:34 -0400

    Add cors

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import morgan from 'morgan'
 import bodyParser from 'body-parser'
+import cors from 'cors'
 import routes from './routes'
 import './db'
 
 const app = express()
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(routes)
