d9a56bd6242dd80192bbad4e6f108634468c4ba0
Alex Miles <alex@studiomile.com>
2018-06-25 15:29:31 +0200

    Increase cache to three minutes

--- routes/api.js
+++ routes/api.js
@@ -9,7 +9,7 @@ import cache from '../middleware/cache'
 
 const router = express.Router()
 
-router.get('/current', authenticate, cache(30), async (req, res) => {
+router.get('/current', authenticate, cache(180), async (req, res) => {
   try {
     const query = `
     SELECT
