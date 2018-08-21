910d7f0492e0fe0e3673a9c74e00209eec349584
alex miles <alex@studiomile.com>
2018-08-21 15:39:33 -0400

    Fix range (#36)
    * Add two prices to seed
    
    * Fix test and rang endpoint
    
    * Order by timestamp

--- routes/api.js
+++ routes/api.js
@@ -61,6 +61,7 @@ router.get('/range', authenticate, async (req, res) => {
       .eager('prices')
       .modifyEager('prices', builder => {
         builder.whereBetween('timestamp', [start, end])
+        builder.orderBy('timestamp')
       })
 
     const response = coins.reduce((acc, val) => {
